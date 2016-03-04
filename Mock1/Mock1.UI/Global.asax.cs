54e41662347992a7ede137db65ff6fda4974f83a
Timothy Carleo <timothy.joseph@outlook.com>
2016-03-03 20:51:19 -0500

    .

---
+++ Mock1/Mock1.UI/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Mock1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default",                                              // Route name
+                "{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
+            );
+
+        }
+    }
+}
